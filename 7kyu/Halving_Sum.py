9ce9745f0cc8a51dfec2885686caafee580e2cba
Pham Nhat Huy <huy.pham@cmetric.vn>
2021-08-10 00:40:15 +0700

    Update kata

---
+++ 7kyu/Halving_Sum.py
@@ -0,0 +1,18 @@
+'''
+Task
+Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+'''
+
+
+def halving_sum(n): 
+    sum = 0
+    while n != 0:
+        sum+=n
+        n = n // 2
+    return sum
